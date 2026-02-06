cd30bfd647f5ec84dcd6ca1b942001f9cd5bb59c
Nayarid Rengel <nayariddelvalle0620@gmail.com>
2026-02-06 17:34:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//Este es un comentario: Mi primer programa de lógica 
+let racha = 400; // Aquí guardamos tu racha de duolingo     
+if (racha >= 365) {
+     console.log("¡Felicidades, Olga! Tienes disiplina de ingeniera. ");
+     alert("Eres una genia de la constancia sigue así, Olga.");
+     alert("¡Más de un año de constancia! Eres increible.");
+} else {
+     console.log("Sigue practicando, cada día cuenta.");
+}   
